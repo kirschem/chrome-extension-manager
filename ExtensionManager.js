8fd42ea901de5daea328d9bd8e86739a0ad2e612
Kirsche <MKirsche@testotis.de>
2020-01-14 10:31:52 +0100

    Refactoring: Pull methods to abstract ExtensionManager

--- ExtensionManager.js
+++ ExtensionManager.js
@@ -1,4 +1,11 @@
+const _fs = require("fs");
+const path = require("path");
+const { app } = require("electron");
+const fs = _fs.promises;
+
 class ExtensionManager {
+  _localesFolder = "_locales";
+
   constructor(extensionDir, policiesDir) {
     if (new.target === ExtensionManager) {
       throw new TypeError(
@@ -19,6 +26,55 @@ class ExtensionManager {
 
   async disableExtension(id) {
     throw new TypeError("Must override method");
+  }
+
+  _getLocale() {
+    return app.getLocale();
+  }
+
+  async _retrieveIconAsBase64DataUri(iconPath) {
+    const ext = path.extname(iconPath).substr(1);
+    const base64 = await fs.readFile(iconPath, { encoding: "base64" });
+    return `data:image/${ext};base64,${base64}`;
+  }
+
+  async _retrieveTranslation(key, extensionPath) {
+    const localesPath = path.join(extensionPath, this._localesFolder);
+    key = key.replace("MSG_", "").replace(/__/g, "");
+
+    // Some translations are lowercase regardless of caml case key, some respect case sensitivity
+    const bufferToTranslation = buffer => {
+      const translations = JSON.parse(buffer);
+      return translations[key]
+        ? translations[key].message
+        : translations[key.toLowerCase()].message;
+    };
+
+    try {
+      // Try user's current locale first
+      await fs
+        .readFile(path.join(extensionPath, this._getLocale, "messages.json"))
+        .then(bufferToTranslation);
+    } catch (error) {
+      // Otherwise get the first english version we can find
+      const englishLocaleFolder = await (
+        await fs.readdir(localesPath)
+      ).find(locale => locale.startsWith("en"));
+      if (englishLocaleFolder) {
+        return await fs
+          .readFile(
+            path.join(localesPath, englishLocaleFolder, "messages.json")
+          )
+          .then(bufferToTranslation);
+      }
+    }
+
+    // Return back the key if there was no appropriate translation found
+    console.warn(
+      "Could not find appropriate translation for extension in path: %s",
+      extensionPath
+    );
+    return key;
   }
 }
 
--- WindowsExtensionManager.js
+++ WindowsExtensionManager.js
@@ -1,61 +1,9 @@
 const _fs = require("fs");
-const ExtensionManager = require("./ExtensionManager");
 const path = require("path");
-const { app } = require("electron");
 const fs = _fs.promises;
+const ExtensionManager = require("./ExtensionManager");
 
 class WindowsExtensionManager extends ExtensionManager {
-  _localesFolder = "_locales";
-
-  _getLocale() {
-    return app.getLocale();
-  }
-
-  async _retrieveTranslation(key, extensionPath) {
-    const localesPath = path.join(extensionPath, this._localesFolder);
-    key = key.replace("MSG_", "").replace(/__/g, "");
-
-    // Some translations are lowercase regardless of caml case key, some respect case sensitivity
-    const bufferToTranslation = buffer => {
-      const translations = JSON.parse(buffer);
-      return translations[key]
-        ? translations[key].message
-        : translations[key.toLowerCase()].message;
-    };
-
-    try {
-      // Try user's current locale first
-      await fs
-        .readFile(path.join(extensionPath, this._getLocale, "messages.json"))
-        .then(bufferToTranslation);
-    } catch (error) {
-      // Otherwise get the first english version we can find
-      const englishLocaleFolder = await (
-        await fs.readdir(localesPath)
-      ).find(locale => locale.startsWith("en"));
-      if (englishLocaleFolder) {
-        return await fs
-          .readFile(
-            path.join(localesPath, englishLocaleFolder, "messages.json")
-          )
-          .then(bufferToTranslation);
-      }
-    }
-
-    // Return back the key if there was no appropriate translation found
-    console.warn(
-      "Could not find appropriate translation for extension in path: %s",
-      extensionPath
-    );
-    return key;
-  }
-
-  async _retrieveIconAsBase64DataUri(iconPath) {
-    const ext = path.extname(iconPath).substr(1);
-    const base64 = await fs.readFile(iconPath, { encoding: "base64" });
-    return `data:image/${ext};base64,${base64}`;
-  }
-
   async listExtensions() {
     const extensionIds = await fs.readdir(this._extensionDir);
     return Promise.all(
