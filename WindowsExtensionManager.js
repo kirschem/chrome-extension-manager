f1a4b21ced7156cabdfaaad69c435c7ec0999984
Kirsche <MKirsche@testotis.de>
2020-01-14 13:43:23 +0100

    Resiliency against errors during extension listing

--- WindowsExtensionManager.js
+++ WindowsExtensionManager.js
@@ -8,43 +8,55 @@ class WindowsExtensionManager extends ExtensionManager {
     const extensionIds = await fs.readdir(this._extensionDir);
     return Promise.all(
       extensionIds.map(async id => {
-        const versionFolder = (
-          await fs.readdir(path.join(this._extensionDir, id))
-        )[0];
+        try {
+          // Get the version folder first, that contains the extension's src
+          const versionFolder = (
+            await fs.readdir(path.join(this._extensionDir, id))
+          )[0];
 
-        const manifest = await fs
-          .readFile(
-            path.join(this._extensionDir, id, versionFolder, "manifest.json")
-          )
-          .then(buffer => JSON.parse(buffer));
-
-        // Get translated extension name
-        if (manifest["name"].startsWith("__")) {
-          manifest["name"] = await this._retrieveTranslation(
-            manifest["name"],
-            path.join(this._extensionDir, id, versionFolder)
-          );
-        }
-
-        const iconSrc = manifest["icons"]
-          ? await this._retrieveIconAsBase64DataUri(
-              path.join(
-                this._extensionDir,
-                id,
-                versionFolder,
-                manifest["icons"]["128"]
-              )
+          // Get the manifest
+          const manifest = await fs
+            .readFile(
+              path.join(this._extensionDir, id, versionFolder, "manifest.json")
             )
-          : "";
+            .then(buffer => JSON.parse(buffer));
 
-        return {
-          name: manifest["name"],
-          version: manifest["version"],
-          id: id,
-          iconSrc: iconSrc
-        };
+          // Get translated extension name
+          if (manifest["name"].startsWith("__")) {
+            manifest["name"] = await this._retrieveTranslation(
+              manifest["name"],
+              path.join(this._extensionDir, id, versionFolder)
+            );
+          }
+
+          // Get the icon
+          const iconSrc = manifest["icons"]
+            ? await this._retrieveIconAsBase64DataUri(
+                path.join(
+                  this._extensionDir,
+                  id,
+                  versionFolder,
+                  manifest["icons"]["128"]
+                )
+              )
+            : "";
+
+          return {
+            name: manifest["name"],
+            version: manifest["version"],
+            id: id,
+            iconSrc: iconSrc
+          };
+        } catch (error) {
+          console.error(
+            "Could not process extension with id '%s': %o",
+            id,
+            error
+          );
+          return null;
+        }
       })
-    );
+    ).then(results => results.filter(ext => ext !== null));
   }
 }
 
