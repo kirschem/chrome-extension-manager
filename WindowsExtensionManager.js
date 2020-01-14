49ba675027effd2c855a5e8da04086a78d24841f
Kirsche <MKirsche@testotis.de>
2020-01-14 17:08:35 +0100

    Pushed methods to base class

--- WindowsExtensionManager.js
+++ WindowsExtensionManager.js
@@ -3,61 +3,6 @@ const path = require("path");
 const fs = _fs.promises;
 const ExtensionManager = require("./ExtensionManager");
 
-class WindowsExtensionManager extends ExtensionManager {
-  async listExtensions() {
-    const extensionIds = await fs.readdir(this._extensionDir);
-    return Promise.all(
-      extensionIds.map(async id => {
-        try {
-          // Get the version folder first, that contains the extension's src
-          const versionFolder = (
-            await fs.readdir(path.join(this._extensionDir, id))
-          )[0];
-
-          // Get the manifest
-          const manifest = await fs
-            .readFile(
-              path.join(this._extensionDir, id, versionFolder, "manifest.json")
-            )
-            .then(buffer => JSON.parse(buffer));
-
-          // Get translated extension name
-          if (manifest["name"].startsWith("__")) {
-            manifest["name"] = await this._retrieveTranslation(
-              manifest["name"],
-              path.join(this._extensionDir, id, versionFolder)
-            );
-          }
-
-          // Get the icon
-          const iconSrc = manifest["icons"]
-            ? await this._retrieveIconAsBase64DataUri(
-                path.join(
-                  this._extensionDir,
-                  id,
-                  versionFolder,
-                  manifest["icons"]["128"]
-                )
-              )
-            : "";
-
-          return {
-            name: manifest["name"],
-            version: manifest["version"],
-            id: id,
-            iconSrc: iconSrc
-          };
-        } catch (error) {
-          console.error(
-            "Could not process extension with id '%s': %o",
-            id,
-            error
-          );
-          return null;
-        }
-      })
-    ).then(results => results.filter(ext => ext !== null));
-  }
-}
+class WindowsExtensionManager extends ExtensionManager {}
 
 module.exports = WindowsExtensionManager;
